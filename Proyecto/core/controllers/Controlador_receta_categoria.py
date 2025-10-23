325bdf7dc01f33956bba8bee67fda3b77d8c6f22
unknown <jbeltrano@unal.edu.co>
2025-10-23 13:08:35 -0500

    Implementacion de la seleccion de categorias

--- Proyecto/core/controllers/Controlador_receta_categoria.py
+++ Proyecto/core/controllers/Controlador_receta_categoria.py
@@ -7,7 +7,7 @@ def obtener_recetas_categorias():
     return Receta_categoria.objects.all()
 
 def obtener_receta_categoria(categoria_id):
-    return Receta_categoria.objects.filter(categoria_id=categoria_id).first()
+    return Receta_categoria.objects.filter(categoria_id=categoria_id)
 
 def obtener_categorias_de_receta(receta_id):
     return Receta_categoria.objects.filter(receta_id=receta_id)
@@ -24,4 +24,20 @@ def insertar_receta_categoria(receta_id, categoria_id):
         receta=receta,
         categoria=categoria
     )
+
+def eliminar_receta_categoria(receta_id, categoria_id):
+    return Receta_categoria.objects.filter(
+        receta_id=receta_id,
+        categoria_id=categoria_id
+    ).delete()
+
+def obtener_recetas_por_categoria(categoria_id):
+    """
+    Obtiene todas las recetas que pertenecen a una categoría específica.
+    Args:
+        categoria_id: ID de la categoría
+    Returns:
+        QuerySet de objetos Receta que pertenecen a la categoría especificada
+    """
+    return Receta.objects.filter(receta_categoria__categoria_id=categoria_id)
 
--- Proyecto/core/views/Receta_view.py
+++ Proyecto/core/views/Receta_view.py
@@ -1,26 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from core.controllers.Controlador_receta import obtener_recetas, obtener_receta
+from core.controllers.Controlador_categoria import obtener_categorias
+from core.controllers.Controlador_receta_categoria import obtener_recetas_por_categoria
 from django.db.models import Q
 
+
 def lista_recetas(request):
-    # Obtener el término de búsqueda
+    # Obtener el término de búsqueda y la categoría seleccionada
     query = request.GET.get('q', '')
+    categoria_id = request.GET.get('categoria', '')
     
-    # Obtener todas las recetas
-    recetas = obtener_recetas()
+    # Obtener todas las categorías para el dropdown
+    categorias = obtener_categorias()
+    
+    # Obtener las recetas según la categoría seleccionada
+    if categoria_id and categoria_id.isdigit():
+        recetas = obtener_recetas_por_categoria(int(categoria_id))
+    else:
+        recetas = obtener_recetas()
     
     # Aplicar filtro si hay término de búsqueda
     if query:
         recetas = recetas.filter(
             Q(nombre__icontains=query) |
             Q(descripcion__icontains=query) |
-            Q(ingredientes__icontains=query)
+            Q(ingredientes__icontains=query) |
+            Q(porcion__icontains=query)
         )
     
     context = {
         'recetas': recetas,
-        'query': query
+        'query': query,
+        'categorias': categorias,
+        'categoria_seleccionada': categoria_id
     }
     return render(request, 'recetas/lista_recetas.html', context)
 
