179038738cf7d3e7246a73c25909e8848faa14dd
Juan Beltran <jbeltrano@unal.edu.co>
2025-11-06 21:59:50 -0500

    gitignore y correccion de los test

--- Proyecto/core/tests/test_login.py
+++ Proyecto/core/tests/test_login.py
@@ -2,41 +2,39 @@ import pytest
 from core.controllers.Controlador_validaciones import validar_obligatoriedad
 from core.controllers.Controlador_validaciones import validar_correo
 from core.controllers.Controlador_validaciones import validar_longitud_contraseña
-from core.controllers.Controlador_validaciones import validar_contraseña
 from core.controllers.Controlador_login import verificar_registro
-from django.contrib.auth.hashers import make_password
 
 @pytest.mark.parametrize(
     "nombre, apellido, correo, contraseña, contraseña_confirmada, resultado",
     [
-        ("Juan", "Beltran","juan@gmail.com","12345678","12345678", True),
-        ("", "Beltran","juan@gmail.com","12345678","12345678", False),
-        ("Juan", "","juan@gmail.com","12345678","12345678", False),
-        ("Juan", "Beltran","","12345678","12345678", False),
-        ("Juan", "Beltran","juan@gmail.com","","12345678", False),
-        ("Juan", "Beltran","juan@gmail.com","12345678","", False),
+        ("Todos", "los","campos","tienen","valor", True),
+        ("", "falta","un","unico","campo", False),
+        ("falta", "","un","campo","en otro lugar", False),
+        ("falta", "un","","campo","en otro lugar", False),
+        ("falta", "un","campo","","en otro lugar", False),
+        ("falta", "un","campo","en otro lugar","", False),
         ("", "","","","", False),
-        ("", "","juan@gmail.com","12345678","12345678", False),
-        ("", "Beltran","","12345678","", False),
+        ("", "","falta","mas de un","campo", False),
+        ("", "falta mas de un","","campo","", False),
+        (" ", "falta mas de un"," ","campo","", False),
     ]
 )
 
 def test_verificar_registro_campos_obligatorios(nombre, apellido, correo, contraseña, contraseña_confirmada, resultado):
-    
     assert validar_obligatoriedad([nombre, apellido, correo, contraseña, contraseña_confirmada]) == resultado
 
 
 @pytest.mark.parametrize(
     "correo, resultado",
     [
-        ("juan@gmail.com", True),
+        ("correo@valido.com", True),
         ("correo_invalido", False),
         ("", False),
-        ("juan@", False),
-        ("juan@gmail", False),
-        ("juan@gmail.c", False),
-        ("juan@.com", False),
-        ("juan@yahoo.es", True),
+        ("sin_dominio_ni_extencion@", False),
+        ("sin@extencion", False),
+        ("extencion@incompleta.c", False),
+        ("sin_dominio@.com", False),
+        ("con espacio@yahoo.es", False),
     ]
 )
 
@@ -46,15 +44,9 @@ def test_validar_correo(correo, resultado):
 @pytest.mark.parametrize(
     "password, resultado",
     [
-        ("12345678", True),
-        ("asdfghjk", True),
-        ("1a2s3d4f", True),
-        ("laksjdfñlkaj123423asdfas", True),
-        ("1234567", False),
-        ("abcd123", False),
-        ("123", False),
-        ("abc", False),
-        ("", False),
+        ("es suficientemente larga", True),
+        ("escorta", False),
+        ("8 exacto", True),
     ]
 )
 
@@ -64,30 +56,13 @@ def test_validar_longitud_contraseña(password, resultado):
 @pytest.mark.parametrize(
     "nombre, apellido, correo, contraseña, contraseña_confirmada, resultado",
     [
-        ("Juan", "Beltran","juan@gmail.com","12345678","12345678", []),
-        ("", "Beltran","juan@gmail.com","12345678","12345678", ["Todos los campos son obligatorios."]),
-        ("Juan", "Beltran","juan@.com","asdfghjk","asdfghjk", ["El correo electrónico no es válido."]),
-        ("Juan", "Beltran","juan@gmail.com","1234","1234", ["La contraseña debe tener al menos 8 caracteres."]),
-        ("Juan", "Beltran","juan@gmail.com","1a2b345678","1a2b345678as", ["Las contraseñas no coinciden."]),
+        ("todos", "los","camopos@estan.com","completos","completos", []),
+        ("", "falta","algun@campo.com","incompleto","incompleto", ["Todos los campos son obligatorios."]),
+        ("El", "correo","no@.com","es valido","es valido", ["El correo electrónico no es válido."]),
+        ("La", "contraseña","es@gmail.com","corta","corta", ["La contraseña debe tener al menos 8 caracteres."]),
+        ("las", "contraseñas","son@gmail.com","igualiticas","no son igualiticas", ["Las contraseñas no coinciden."]),
     ]
 )
 
 def test_verificar_registro(nombre, apellido, correo, contraseña, contraseña_confirmada, resultado):
     assert verificar_registro(nombre, apellido, correo, contraseña, contraseña_confirmada) == resultado
-
-
-@pytest.mark.parametrize(
-    "contraseña_ingresada, contraseña_db, resultado",
-    [
-        ("12345678",make_password("12345678"), True),
-        ("password", make_password("password"), True),
-        ("wrongpassword", make_password("12345678"), False),
-        ("anotherwrong", make_password("password"), False),
-        ("", make_password("password"), False),
-        ("12345678", "", False),
-        ("12345678", "12345678", False),
-    ]
-)
-
-def test_validar_contraseña(contraseña_ingresada, contraseña_db, resultado):
-    assert validar_contraseña(contraseña_ingresada, contraseña_db) == resultado
